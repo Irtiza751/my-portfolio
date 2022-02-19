f467a99d271d2b6ed3d3eb073d9079580de2c663
Muhammad Irtiza <muhammadirtiza@pop-os.localdomain>
2022-02-19 14:38:53 +0500

    Animation added in header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { GitHub, Instagram, Linkedin } from 'react-feather';
+import Image from "next/image";
+import Link from "next/link";
+import { GitHub, Instagram, Linkedin } from "react-feather";
+import { AnimatePresence, motion } from "framer-motion";
 
 const links = [
   { name: "Projects", anchor: "#projects" },
@@ -11,46 +12,99 @@ const links = [
 
 export default function Header() {
   return (
-    <header className="border-b-2 border-gray-400/10 py-8 mb-2">
-      <nav className="container flex justify-between">
-        <div className="w-44">
-          <Link href="/">
-            <a>
-              <Image src="/logo.svg" alt="Muhammad Irtiza" width={250} height={40} />
-            </a>
-          </Link>
-        </div>
+    <AnimatePresence>
+      <header className="border-b-2 border-gray-400/10 py-8 mb-2">
+        <nav className="container flex justify-between">
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            animate={{ opacity: 1, y: 0, transition: { delay: 0.2 } }}
+          >
+            <div className="w-44">
+              <Link href="/">
+                <a>
+                  <Image
+                    src="/logo.svg"
+                    alt="Muhammad Irtiza"
+                    width={250}
+                    height={40}
+                  />
+                </a>
+              </Link>
+            </div>
+          </motion.div>
 
-        <ul className='lg:flex gap-x-8 text-lg hidden'>
-          {links.map((link, i) => (
-            <li key={i}><Link href={link.anchor}><a className='hover:text-gray-200'>{link.name}</a></Link></li>
-          ))}
-        </ul>
+          <ul className="lg:flex gap-x-8 text-lg hidden">
+            {links.map((link, i) => (
+              <motion.li
+                key={i}
+                initial={{ opacity: 0, y: -10 }}
+                animate={{
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    delay: i * 0.1,
+                  },
+                }}
+              >
+                <Link href={link.anchor}>
+                  <a className="hover:text-gray-200">{link.name}</a>
+                </Link>
+              </motion.li>
+            ))}
+          </ul>
 
-        <ul className='flex gap-x-8'>
-          <li>
-            <Link href="https://github.com/Irtiza751/">
-              <a target="_blank" className='hover:text-gray-200'>
-                <GitHub size={24} />
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://www.linkedin.com/in/muhammad-irtiza-b68843211/">
-              <a className='hover:text-gray-200' target="_blank">
-                <Linkedin size={24} />
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://www.instagram.com/muhammad_irtiza751/">
-              <a className='hover:text-gray-200' target="_blank">
-                <Instagram size={24} />
-              </a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+          <ul className="flex gap-x-8">
+            <motion.li
+              initial={{ opacity: 0, y: -10 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 0.1,
+                },
+              }}
+            >
+              <Link href="https://github.com/Irtiza751/">
+                <a target="_blank" className="hover:text-gray-200">
+                  <GitHub size={24} />
+                </a>
+              </Link>
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0, y: -10 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 0.2,
+                },
+              }}
+            >
+              <Link href="https://www.linkedin.com/in/muhammad-irtiza-b68843211/">
+                <a className="hover:text-gray-200" target="_blank">
+                  <Linkedin size={24} />
+                </a>
+              </Link>
+            </motion.li>
+            <motion.li
+              initial={{ opacity: 0, y: -10 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: {
+                  delay: 0.3,
+                },
+              }}
+            >
+              <Link href="https://www.instagram.com/muhammad_irtiza751/">
+                <a className="hover:text-gray-200" target="_blank">
+                  <Instagram size={24} />
+                </a>
+              </Link>
+            </motion.li>
+          </ul>
+        </nav>
+      </header>
+    </AnimatePresence>
   );
 }
